313201165b6a228c01a4c83a601139e6123150ee
TejaAncha <55844009+TejaAncha@users.noreply.github.com>
2020-04-26 22:32:54 +0800

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -2,10 +2,11 @@ from netmiko import ConnectHandler as ch
 from getpass import getpass
 userName = input("UserName: ")
 password = getpass("password: ")
+command = input("command: ")
 get_ip = input("provide ip address: ")
 device_details = [{"device_type":"cisco_ios","ip":get_ip, "username":userName, "password":password}]
 connect = ch(**device_details[0])
-out = connect.send_command("sh ip int br")
+out = connect.send_command(command)
 try:
     for l in range(0,len(device_details)):
         with open(f"device{int(l+1)}", "w") as file:
