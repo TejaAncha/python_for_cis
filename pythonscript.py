03338b22afed1f4b70e1250bed39db8130d7b9f3
Teja Ancha <anchadharaniteja@outlook.com>
2020-04-26 15:14:43 -0700

    upgraded by Teja

--- pythonscript.py
+++ pythonscript.py
@@ -3,11 +3,12 @@ from getpass import getpass
 userName = input("UserName: ")
 password = getpass("password: ")
 get_ip = input("provide ip address: ")
-device_details = {"device_type":"cisco_ios","ip":get_ip, "username":userName, "password":password}
-connect = ch(**device_details)
+device_details = [{"device_type":"cisco_ios","ip":get_ip, "username":userName, "password":password}]
+connect = ch(**device_details[0])
 out = connect.send_command("sh ip int br")
 try:
-    with open("Device1", "w") as file:
-        file.write(out)
+    for l in range(0,len(device_details)):
+        with open(f"device{int(l+1)}", "w") as file:
+            file.write(out)
 except:
     print("No such file found")
